dedb8d968bb16c896e6c3f0dd5cec131ee5f4e3b
IsaacAvila29 <isaac.avila.saenz29@gmail.com>
2025-04-25 20:20:05 -0600

    feat: implement BibliographyForm with radio selection for reference type

--- src/app/components/Bibliography/BibliographyForm.tsx
+++ src/app/components/Bibliography/BibliographyForm.tsx
@@ -1,6 +1,57 @@
+"use client";
+import {
+  Box,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+  TextField,
+} from "@mui/material";
 import React from "react";
 
+const BibliographyBook = () => {
+  return (
+    <>
+      <TextField label="Autor/a (Apellido, Inicial)" fullWidth />
+      <TextField label="Año de publicación" fullWidth />
+      <TextField label="Título del libro" fullWidth />
+      <TextField label="Editorial" fullWidth />
+    </>
+  );
+};
+
+const BibliographyWeb = () => {
+  return (
+    <>
+      <TextField label="Autor/a o nombre del sitio" fullWidth />
+      <TextField label="Fecha (Año, día mes)" fullWidth />
+      <TextField label="Título de la página" fullWidth />
+      <TextField label="Nombre del sitio web" fullWidth />
+      <TextField label="URL" fullWidth />
+    </>
+  );
+};
+
 export const BibliographyForm = () => {
-  return <div>BibliographyForm</div>;
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log((event.target as HTMLInputElement).value);
+  };
+  return (
+    <FormControl>
+      <FormLabel id="demo-radio-buttons-group-label">
+        Tipo de referencia
+      </FormLabel>
+      <RadioGroup
+        aria-labelledby="demo-radio-buttons-group-label"
+        defaultValue="web"
+        name="radio-buttons-group"
+        onChange={handleChange}
+      >
+        <FormControlLabel value="web" control={<Radio />} label="Página Web" />
+        <FormControlLabel value="book" control={<Radio />} label="Libro" />
+      </RadioGroup>
+    </FormControl>
+  );
 };
 export default BibliographyForm;
---
+++ src/app/playground/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import BibliographyForm from "../components/Bibliography/BibliographyForm";
+
+export const page = () => {
+  return <BibliographyForm />;
+};
+export default page;
